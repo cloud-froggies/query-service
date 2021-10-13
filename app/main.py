7e43e843c2a8bf6e70b2aac107e342cf12a818ba
lsophiagr <lsophiagr@gmail.com>
2021-10-13 16:51:44 -0600

    changes to click service

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import Optional, List
 from fastapi import FastAPI, HTTPException
 from fastapi.logger import logger
 from fastapi.param_functions import Query
+from fastapi.responses import RedirectResponse
 import json
 
 import logging
@@ -13,34 +14,22 @@ import datetime
 import pymysql
 import http.client
 
-
-
 DB_ENDPOINT = os.environ.get('db_endpoint')
 DB_ADMIN_USER = os.environ.get('db_admin_user')
 DB_ADMIN_PASSWORD = os.environ.get('db_admin_password')
 DB_NAME = os.environ.get('db_name')
 
-app = FastAPI(title='Query Service', version='0.1')
+app = FastAPI(title='Click Service', version='0.1')
 gunicorn_logger = logging.getLogger('gunicorn.error')
 logger.handlers = gunicorn_logger.handlers
 logger.setLevel(gunicorn_logger.level)
 
-query_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/query'
-matching_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/matching'
-exclusion_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/exclusion'
-targeting_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/targeting'
-ranking_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/ranking'
-ads_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/ads'
-pricing_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/pricing'
-click_endpoint = 'http://public-18635190.us-east-2.elb.amazonaws.com/click'
-tracking_query_endpoint = 'internal-private-1191134035.us-east-2.elb.amazonaws.com/tracking/query'
-tracking_impression_endpoint = 'internal-private-1191134035.us-east-2.elb.amazonaws.com/tracking/impression'
+tracking_click_endpoint = 'http://internal-private-1191134035.us-east-2.elb.amazonaws.com/tracking/click'
 
 if __name__ != "main":
     logger.setLevel(gunicorn_logger.level)
 else:
     logger.setLevel(logging.DEBUG)
-
 
 def get_db_conn():
     try:
@@ -51,172 +40,121 @@ def get_db_conn():
         logger.error(e)
         raise
     
-    
-def put_items(items):
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
-    table = dynamodb.Table('sessions')
-
-    with table.batch_writer() as batch:
-        for item in items:
-            batch.put_item(
-                Item=item
-            )
-    return table
-
-
 @app.get("/")
 def read_root():
-    table = put_items([])
-    return {"Service": "Query", "table.creation_date_time": table.creation_date_time, 'tomorrow': datetime.datetime.now().timestamp() + (24*60*60)}
+    return {"Service": "Click"}
+
+@app.get("/table")
+def table():
+    client = boto3.client('dynamodb')
+
+    response = client.describe_table(
+        TableName='sessions'
+    )
+
+    return {"Service": response}
+
+# @app.get("/click", response_class=RedirectResponse, status_code=302)
+# async def click(query_id: str, impression_id: str):
+
+    # conn = get_db_conn()
+    # now_timestamp_iso = datetime.datetime.now().isoformat()
+    
+    # # click
+    # dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    # table = dynamodb.Table('sessions')
+    # response = table.get_item(
+    #     Key={
+    #         'query_id': query_id,
+    #         'impression_id': impression_id
+    #     }
+    # )
+    # advertiser_url = response['Item']['advertiser_url']
+
+    # # track click
+    # click_id = str(uuid.uuid4())
+    
+    # # using db connection
+    # with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+    #     sql_query = """SELECT campaign_id FROM ads WHERE advertiser_url = %s"""
+    #     cursor.execute(sql_query, (advertiser_url))
+    #     campaign_id = cursor.fetchone()
+        
+    #     sql_query = """SELECT advertiser_id FROM advertiser_campaigns WHERE id = %s"""
+    #     cursor.execute(sql_query, (campaign_id))
+    #     advertiser_id = cursor.fetchone()
+        
+    #     sql_query = """SELECT publisher_id FROM publisher_exclusions WHERE advertiser_id = %s"""
+    #     cursor.execute(sql_query, (advertiser_id))
+    #     publisher_id = cursor.fetchone()
+        
+    #     sql_query4 = """SELECT category FROM advertiser_campaigns WHERE id = %s"""
+    #     cursor.execute(sql_query, (campaign_id))
+    #     category = cursor.fetchone()
+        
+    #     sql_query = """SELECT id FROM ads WHERE url = %s"""
+    #     cursor.execute(sql_query, (advertiser_url))
+    #     ad_id = cursor.fetchone()
+        
+    #     sql_query6 = """SELECT zip_code FROM campaign_targeting WHERE campaign_id = %s"""
+    #     cursor.execute(sql_query6, (campaign_id))
+    #     zip_code = cursor.fetchone()
+    
+    # tracking_click_params = {
+    #     "query_id": query_id,
+    #     "impression_id": impression_id,
+    #     "click_id": click_id,
+    #     "timestamp": now_timestamp_iso,
+    #     "publisher_id": publisher_id,
+    #     "advertiser_id": advertiser_id,
+    #     "advertiser_campaign_id": campaign_id,
+    #     "category": category,
+    #     "ad_id": ad_id,
+    #     "zip_code": str(zip_code),
+    #     "advertiser_price": 0.0,
+    #     "publisher_price": 0.0,
+    #     "position": 0
+    # }
+    
+    # tracking_click_response = requests.get(tracking_click_endpoint, params=tracking_click_params)
+    # logger.error(tracking_click_response)
+
+    # return advertiser_url
 
 
-@app.get("/query")
-def query(category: int, publisher: int, zip_code: int, maximum: int = None):
+@app.get("/click", response_class=RedirectResponse, status_code=302)
+async def click(query_id: str, impression_id: str):
     try:
-
-        now_timestamp = datetime.datetime.now()
-
         conn = get_db_conn()
-        
-        # matching
-        matching_params = {"category": category}
-        matching_response = requests.get(matching_endpoint, params=matching_params)
-        matching_response.raise_for_status()
-        logger.error(matching_response)
-        logger.error(matching_response.json())
-        campaign_ids = [campaign['id'] for campaign in matching_response.json()]
-        advertiser_campaigns = ','.join(map(str, campaign_ids))
-        logger.error(advertiser_campaigns)
+        now_timestamp_iso = datetime.datetime.now().isoformat()
 
-        # exclusion & targeting
-        exclusion_params = {"advertiser_campaigns": advertiser_campaigns, 'publisher': publisher}
-        exclusion_response = requests.get(exclusion_endpoint, exclusion_params)
+        # track click
+        click_id = str(uuid.uuid4())
 
-        targeting_params = {"advertiser_campaigns": advertiser_campaigns, "zip_code": zip_code}
-        targeting_response = requests.get(targeting_endpoint, targeting_params)
-
-        logger.error(exclusion_response.json())
-        logger.error(targeting_response.json())
-
-        valid_campaigns = list(set(exclusion_response.json()).intersection(set(targeting_response.json())))
-        valid_advertiser_campaigns = ','.join(map(str, valid_campaigns))
-        logger.error(valid_campaigns)
-        logger.error(valid_advertiser_campaigns)
-
-        campaign_bids = [campaign['bid'] for campaign in matching_response.json() if (campaign['id'] in valid_campaigns)]
-        campaign_bids = ','.join(map(str, campaign_bids))
-
-        # ranking
-        ranking_params = {"advertiser_campaigns": valid_advertiser_campaigns, "advertiser_campaigns_bids": campaign_bids}
-        if(maximum):
-            ranking_params["maximum"] = maximum
-
-        ranking_response = requests.get(ranking_endpoint, params=ranking_params)
-        logger.error(ranking_response.json())
-
-        # ads
-        ads_params = {"advertiser_campaigns": valid_advertiser_campaigns}
-        ads_response = requests.get(ads_endpoint, params=ads_params)
-        logger.error(ads_response.json())
-
-        # pricing 
-        pricing_params = {"advertiser_campaigns": valid_advertiser_campaigns, "advertiser_campaigns_bids": campaign_bids, "publisher": publisher}
-        pricing_response = requests.get(pricing_endpoint, params=pricing_params)
-        logger.error(pricing_response.json())
-
-        # query & tracking (impression events)
-        query_id = str(uuid.uuid4())
-        response_ads_aray = []
-        dynamo_ads_array = []
-        
-        for index in range(0, len(ads_response.json())):
-            ad = ads_response.json()[index]
-            impression_id = str(uuid.uuid4())
-            response_ads_aray.append({
-                "impression_id": impression_id,
-                "headline": ad["headline"],
-                "description": ad["description"],
-                "click_url": f'{click_endpoint}?query_id={query_id}&impression_id={impression_id}'
-            })
-
-            dynamo_ads_array.append({
-                "query_id": query_id,
-                "impression_id": impression_id,
-                "advertiser_url": ad["url"],
-                # hours * minutes * seconds
-                "expdate": int(now_timestamp.timestamp() + (24*60*60))
-            })
-
-            # using list comprehension
-            # publisher_price = [campaign for campaign in pricing_response if campaign['id']==ad['campaign_id']][0]['price']
-            # advertiser_price = [campaign for campaign in matching_response if campaign['id']==ad['campaign_id']][0]['bid'] - publisher_price
-            publisher_price = 0.0
-            advertiser_price = 0.0
-
-            # using db connection
-            with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-                sql_query = """SELECT advertiser_id FROM advertiser_campaigns WHERE id = %s"""
-                cursor.execute(sql_query,(ad['campaign_id']))
-                advertiser_id = cursor.fetchone()
-            
-            tracking_impression_params = {
-                "query_id": str(query_id),
-                "impression_id": str(impression_id),
-                "timestamp": str(now_timestamp.isoformat()),
-                "publisher_id": str(publisher),
-                "advertiser_id" : str(advertiser_id),
-                "advertiser_campaign_id": str(ad['campaign_id']),
-                "category": str(category),
-                "ad_id": str(ad['id']),
-                "zip_code": str(zip_code),
-                "advertiser_price": str(advertiser_price),
-                "publisher_price": str(publisher_price),
-                "position": str(index)
-            }
-            # tracking_impression_response = requests.post(tracking_impression_endpoint, data=json.dumps(tracking_impression_params))
-
-            conn = http.client.HTTPConnection(host=tracking_impression_endpoint)
-            headers = {'Content-type': 'application/json'}
-            json_data = json.dumps(tracking_impression_params)
-            conn.request('POST', '/', json_data, headers)
-            response = conn.getresponse()
-            response.read().decode()
-            # logger.error(tracking_impression_response.json())
-            # tracking_impression_response.raise_for_status()
-
-
-        query_response = {
-            'headers': {
-                'query_id': query_id
-            },
-            'ads': response_ads_aray
-        }
-        
-        # cache query event
-        dynamo_response = put_items(dynamo_ads_array)
-        logger.error(dynamo_response)
-
-        # tracking (query event)
-        tracking_query_params = {
+        tracking_click_params_dummy = {
             "query_id": str(query_id),
-            "timestamp": str(now_timestamp.isoformat()),
-            "publisher_id": str(publisher), 
-            "category" : str(category),
-            "zip_code": str(zip_code)
+            "impression_id": str('impression_id'),
+            "click_id": click_id,
+            "timestamp": str(now_timestamp_iso),
+            "publisher_id": str(9999),
+            "advertiser_id" : str(9999),
+            "advertiser_campaign_id": str(9999),
+            "category": str(9999),
+            "ad_id": str(9999),
+            "zip_code": str('0000'),
+            "advertiser_price": str(99.99),
+            "publisher_price": str(99.99),
+            "position": str(9999)
         }
-        # tracking_query_response = requests.post(tracking_query_endpoint, data=tracking_query_params)
-
-        conn = http.client.HTTPConnection(host=tracking_query_endpoint)
+        
+        conn = http.client.HTTPConnection(host=tracking_click_endpoint)
         headers = {'Content-type': 'application/json'}
-        json_data = json.dumps(tracking_query_params)
+        json_data = json.dumps(tracking_click_params_dummy)
         conn.request('POST', '/', json_data, headers)
         response = conn.getresponse()
         response.read().decode()
 
-        # tracking_query_response.raise_for_status()
-        # logger.error(tracking_query_response)
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-    return query_response
+    return 'Success post'
